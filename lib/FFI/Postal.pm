6df462472afbac747dc38684d8ef67a07201e40a
LapVeesh <rabbiveesh@gmail.com>
2019-09-16 18:59:27 +0300

    Initial commit

---
+++ lib/FFI/Postal.pm
@@ -0,0 +1,29 @@
+use strict;
+use warnings;
+package FFI::Postal;
+use Exporter::Shiny;
+
+our $VERSION = '0.001';
+
+
+=head1 NAME
+
+FFI::Postal - FFI bindings for libpostal
+
+=head1 AUTHOR
+
+Avishai Goldman L<email:veesh@cpan.org>
+
+=head1 SEE ALSO 
+
+L<FFI::Platypus>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2019, Avishai Goldman L<email:veesh@cpan.org>
+
+This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+=cut
+
+1;
